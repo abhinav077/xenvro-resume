72974fda1ad523669d3783450461f07fb9844a7f
abhinav077 <abhinavsirohi077@gmail.com>
2026-05-17 02:13:11 +0530

    minor change

--- app/components/ui/FileUploader.tsx
+++ app/components/ui/FileUploader.tsx
@@ -1,8 +1,9 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
 
+    const [file, setfile] = useState(second)
     const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
   }, [])
@@ -12,7 +13,21 @@ const FileUploader = () => {
     <div className='w-full gradient-border'>
         <div {...getRootProps()}>
             <input {...getInputProps()} />
-                
+            
+            <div className='space-y-4 cursor-pointer'>
+              <div className='mx-auto w-16 h-16 flex items-center justify-center'>
+                <img src="/icons/info.svg" alt="upload" className='size-20' />
+              </div>
+              {file ? (
+                <div>
+
+                </div>
+              ): (
+                <div>
+                  
+                </div>
+              )}
+            </div>
         </div>
     </div>
   )
